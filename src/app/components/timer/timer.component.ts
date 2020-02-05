38d1898afcd724c8a4ff4d3604047d38cbbdd75d
Dobroslav Radosavljevič <dradosavljevic@monetplus.cz>
2020-02-05 13:00:45 +0100

    reactive timer

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-timer',
-  templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss']
+  selector: "app-timer",
+  templateUrl: "./timer.component.html",
+  styleUrls: ["./timer.component.scss"]
 })
 export class TimerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public timer = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  };
 
   ngOnInit() {
-  }
+    const x = setInterval(() => {
+      const countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
 
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.timer = {
+        days,
+        hours,
+        minutes,
+        seconds
+      };
+
+      if (distance < 0) {
+        clearInterval(x);
+      }
+    }, 1000);
+  }
 }
